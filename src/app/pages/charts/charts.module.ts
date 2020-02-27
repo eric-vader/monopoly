19eb198bcf802341bee2e5e2b65ed8aab05e1b47
livian1107 <livian1107@gmail.com>
2020-02-28 04:31:53 +0800

    deployment

--- src/app/pages/charts/charts.module.ts
+++ src/app/pages/charts/charts.module.ts
@@ -26,6 +26,7 @@ import { EchartsMultipleXaxisComponent } from './echarts/echarts-multiple-xaxis.
 import { EchartsAreaStackComponent } from './echarts/echarts-area-stack.component';
 import { EchartsBarAnimationComponent } from './echarts/echarts-bar-animation.component';
 import { EchartsRadarComponent } from './echarts/echarts-radar.component';
+import { EchartsPieComponent } from './echarts/echarts-pie.component';
 
 const components = [
   ChartjsBarComponent,
@@ -46,6 +47,7 @@ const components = [
   EchartsAreaStackComponent,
   EchartsBarAnimationComponent,
   EchartsRadarComponent,
+  EchartsPieComponent,
 ];
 
 @NgModule({
